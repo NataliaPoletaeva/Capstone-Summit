6f507fbefdf36b9d737ae576cfbe365fb09f4e02
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-18 23:10:48 +0500

    Update after linters' check

--- menu.js
+++ menu.js
@@ -2,10 +2,10 @@ const menuBtn = document.querySelector('.ham-menu');
 const menu = document.querySelector('nav');
 const body = document.querySelector('body');
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('change');
-    if (menu.classList.contains('change') === true) {
-        body.style.overflow = 'hidden';
-    } else {
-        body.style.overflow = 'auto';
-    }
-})
+  menu.classList.toggle('change');
+  if (menu.classList.contains('change') === true) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
+});
