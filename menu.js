2f8bbbb0c82e987a1a46432efbc3ec5ac984224f
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-18 22:55:00 +0500

    Add open and close menu and style it

--- menu.js
+++ menu.js
@@ -1,5 +1,11 @@
-const menuBtn = document.querySelector('.ham-menu')
-const menu = document.querySelector('.nav-items')
+const menuBtn = document.querySelector('.ham-menu');
+const menu = document.querySelector('nav');
+const body = document.querySelector('body');
 menuBtn.addEventListener('click', () => {
-    menu.classList.add('.show')
+    menu.classList.toggle('change');
+    if (menu.classList.contains('change') === true) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = 'auto';
+    }
 })
